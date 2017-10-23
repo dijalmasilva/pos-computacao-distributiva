52be8c80001cb694395c428b16d5549ed4e32440
Dijalma <dijalmacz@gmail.com>
2017-10-23 09:59:44 -0300

    Up solution with RMI.

--- aplicacao-ditribuida-rmi/src/main/java/br/com/dijalmasilva/pos/aplicacao/ditribuida/rmi/Consumer.java
+++ aplicacao-ditribuida-rmi/src/main/java/br/com/dijalmasilva/pos/aplicacao/ditribuida/rmi/Consumer.java
@@ -5,10 +5,27 @@
  */
 package br.com.dijalmasilva.pos.aplicacao.ditribuida.rmi;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dijalmasilva
  */
 public class Consumer {
-    
+
+    public static void main(String[] args) {
+
+        try {
+            Bus bus = new Bus();
+            Hello helloService = (Hello) bus.discovery("Hello");
+            String helloTo = helloService.helloTo("Ricardo Job!");
+            JOptionPane.showMessageDialog(null, helloTo);
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- aplicacao-ditribuida-rmi/src/main/java/br/com/dijalmasilva/pos/aplicacao/ditribuida/rmi/Producer.java
+++ aplicacao-ditribuida-rmi/src/main/java/br/com/dijalmasilva/pos/aplicacao/ditribuida/rmi/Producer.java
@@ -5,41 +5,25 @@
  */
 package br.com.dijalmasilva.pos.aplicacao.ditribuida.rmi;
 
-import java.io.Serializable;
-import java.net.MalformedURLException;
-import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author dijalmasilva
  */
-public class Producer extends UnicastRemoteObject implements Hello, Serializable {
-
-    protected Producer() throws RemoteException {
-        super();
-    }
-
-    @Override
-    public String helloTo(String name) throws RemoteException {
-        return "Olá, " + name + "! \n Bem vindo à aplicação RMI!";
-    }
+public class Producer {
 
     public static void main(String[] args) {
+
         try {
-            Registry registryRMI = LocateRegistry.createRegistry(9091);
-            registryRMI.bind("hello", new Producer());
-            System.out.print("Server is ready!");
+            Bus bus = new Bus();
+            System.out.println("Add implementation Hello Service...");
+            bus.registry("Hello", new HelloImpl());
+            System.out.println("Add implementation was successfully!");
         } catch (RemoteException | AlreadyBoundException ex) {
-            Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Add implementation was error!");
+            ex.printStackTrace();
         }
     }
-
 }
